bb5e615dc9182e0686822e4400ca1d40209d51d4
chibaf <chibaf@mac.com>
2021-06-30 02:01:57 +0900

    Add files via upload

---
+++ single_main2.py
@@ -0,0 +1,12 @@
+import ctypes
+import numpy as np
+import time
+from picosdk.usbtc08 import usbtc08 as tc08
+from picosdk.functions import assert_pico2000_ok
+import sys
+
+from pico_single_class import pico_single
+
+single=pico_single()
+for i in range(0,11):
+  print (single.single())
